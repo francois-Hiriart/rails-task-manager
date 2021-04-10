d4e0834d48f24e6341346799bde5ce16d40d7970
françois hiriart <f.hiriart@skema.edu>
2021-04-10 15:17:41 +0200

    exo 2

--- db/migrate/20210410104052_create_tasks.rb
+++ db/migrate/20210410104052_create_tasks.rb
@@ -3,8 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.string :completed
-      t.string :false
+      t.false :completed
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_092207) do
+ActiveRecord::Schema.define(version: 2021_04_10_104052) do
+
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.string "completed"
-    t.string "false"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
